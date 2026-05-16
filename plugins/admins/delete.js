4724f18453823502e88da1566e8eea7a22dd5835
body2285458 <body28909@gmail.com>
2026-05-16 04:55:27 +0300

    Update reply message for quoted message check

--- plugins/admins/delete.js
+++ plugins/admins/delete.js
@@ -1,5 +1,5 @@
 const handler = async (m, { conn }) => {
-  if (!m.quoted) return m.reply(" 🌹 - رد علي الرساله يا ذكي ")
+  if (!m.quoted) return m.reply(" رد على الرساله يعم متكفرنيش ")
   m.quoted.delete()
  // m.delete()
 };
