d9d43b2d7846cbd1c99e98e78f8f6e0dae214233
body2285458 <body28909@gmail.com>
2026-05-16 03:50:49 +0300

    Fix typo in reply message for empty text

--- plugins/ai/chat.js
+++ plugins/ai/chat.js
@@ -1,7 +1,7 @@
 import { AiChat } from "../../system/utils.js";
 
 const handler = async (m, { conn, text, bot }) => {
-  if (!text) return m.reply(" ايه يا غالي حد في حد مضايقك ولا ايه ");
+  if (!text) return m.reply(" ايه يا غالي في حد مضايقك ولا ايه ");
   const res = await AiChat({ text });
   m.reply(res);
 };
