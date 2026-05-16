1e078fd12e658c25f109423fd7d3ac62740a0d4c
body2285458 <body28909@gmail.com>
2026-05-16 03:59:35 +0300

    Update gift.js

--- plugins/bank/gift.js
+++ plugins/bank/gift.js
@@ -71,8 +71,8 @@ const handler = async (m, { conn, command }) => {
             isForwarded: true,
             forwardingScore: 1,
             forwardedNewsletterMessageInfo: {
-                newsletterJid: '120363225356834044@newsletter',
-                newsletterName: '𝐕𝐈𝐈7 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🕷️',
+                newsletterJid: '120363427228734118@newsletter',
+                newsletterName: '𝑬𝑳𝑩𝑹𝑨𝒁𝑰𝑳𝒀 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🇧🇷⚡️',
                 serverMessageId: 0
             }
         }
