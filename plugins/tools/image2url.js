fca1c3b51e94e64cad5c3b00d140a53b332bd1cb
body2285458 <body28909@gmail.com>
2026-05-16 05:05:56 +0300

    Update newsletter channel name and jid

--- plugins/tools/image2url.js
+++ plugins/tools/image2url.js
@@ -21,12 +21,12 @@ const handler = async (m, { conn, command }) => {
     ],
     mentions: [m.sender],
     newsletter: {
-      name: '𝐕𝐈𝐈7 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🕷️',
-      jid: '120363225356834044@newsletter'
+      name: '𝑬𝑳𝑩𝑹𝑨𝒁𝑰𝑳𝒀 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🇧🇷⚡️',
+      jid: '120363427228734118@newsletter'
     },
     interactiveConfig: {
       buttons_limits: 10,
-      list_title: "𝐕𝐈𝐈7 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🕷️",
+      list_title: "𝑬𝑳𝑩𝑹𝑨𝒁𝑰𝑳𝒀 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🇧🇷⚡️",
       button_title: "Click Here",
       canonical_url: "https://vxv-profile.vercel.app"
     }
