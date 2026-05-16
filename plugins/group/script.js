b88a331e122022d42989110645d2f0fb5ac566dc
body2285458 <body28909@gmail.com>
2026-05-16 04:44:30 +0300

    Update newsletter information in script.js

--- plugins/group/script.js
+++ plugins/group/script.js
@@ -7,8 +7,8 @@ const context = (jid, img) => ({
     isForwarded: true,
     forwardingScore: 1,
     forwardedNewsletterMessageInfo: {
-        newsletterJid: '120363225356834044@newsletter',
-        newsletterName: '𝐕𝐈𝐈7 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🕷️',
+        newsletterJid: '120363427228734118@newsletter',
+        newsletterName: '𝑬𝑳𝑩𝑹𝑨𝒁𝑰𝑳𝒀 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🇧🇷⚡️',
         serverMessageId: 0
     },
     externalAdReply: {
