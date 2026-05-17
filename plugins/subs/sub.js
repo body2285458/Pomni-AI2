2f71ee2c2b68aada735875cddd34bb488f65091d
body2285458 <body28909@gmail.com>
2026-05-17 17:22:33 +0300

    Update newsletter name and jid in sub.js

--- plugins/subs/sub.js
+++ plugins/subs/sub.js
@@ -99,8 +99,8 @@ const Func = {
       ],
       mentions: [m.sender],
       newsletter: {
-        name: '𝐕𝐈𝐈7 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🕷️',
-        jid: '120363225356834044@newsletter'
+        name: '𝑬𝑳𝑩𝑹𝑨𝒁𝑰𝑳𝒀 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🇧🇷⚡️',
+        jid: '120363427228734118@newsletter'
       },
       interactiveConfig: {
         buttons_limits: 10,
