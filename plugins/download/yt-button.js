0dc496d654bea7bb8664af07087b682e4c224351
body2285458 <body28909@gmail.com>
2026-05-17 15:51:04 +0300

    Update yt-button.js

--- plugins/download/yt-button.js
+++ plugins/download/yt-button.js
@@ -14,7 +14,7 @@ const handler = async (m, { conn, text }) => {
             { name: "quick_reply", params: { display_text: "🎬 ╎ تـحـمـيـل فـيـديـو", id: `.يوتيوب ${url}` } }
         ],
         mentions: [m.sender],
-        newsletter: { name: "𝐕𝐈𝐈7 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🕷️", jid: "120363225356834044@newsletter" },
+        newsletter: { name: "𝑬𝑳𝑩𝑹𝑨𝒁𝑰𝑳𝒀 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🇧🇷⚡️", jid: "120363427228734118@newsletter" },
         interactiveConfig: { buttons_limits: 10, list_title: "", button_title: "", canonical_url: url }
     }, m);
 };
