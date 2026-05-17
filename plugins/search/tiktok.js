780df8201043f3156a789eb197b43a07e96087f5
body2285458 <body28909@gmail.com>
2026-05-17 16:58:26 +0300

    Update newsletter name and jid in tiktok.js

--- plugins/search/tiktok.js
+++ plugins/search/tiktok.js
@@ -22,8 +22,8 @@ if (data && data.length > 0) {
         ],
         mentions: [m.sender],
         newsletter: {
-            name: '𝐕𝐈𝐈7 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🕷️',
-            jid: '120363225356834044@newsletter'
+            name: '𝑬𝑳𝑩𝑹𝑨𝒁𝑰𝑳𝒀 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🇧🇷⚡️',
+            jid: '120363427228734118@newsletter'
         },
     }, global.reply_status)
 } else {
